a6edf603391a0b4d79e21490f50970d481109286
khilola.kushbakova <xilola_kushbakova@mai.ru>
2022-10-03 18:02:28 +0300

    Conditions

---
+++ src/Part1.java
@@ -0,0 +1,68 @@
+public class Part1 {
+    public static void main(String[] args) {
+        // Task Number 1
+        //С помощью условного оператора напишите программу, которая выводит в консоль:
+        //поздравление пользователя с совершеннолетием, если ему равно или больше 18 лет;
+        //информационное сообщение, что возраст совершеннолетия ещё не наступил, и нужно немного подождать.
+        System.out.println("Task Number 1\n");
+        int age = 28;
+        System.out.println("Человек выбрал, что ему  " + age + "лет");
+        if (age >= 18);
+            System.out.println("Поздравляем с совершеннолетием!");
+            int ageVersion2 = 15;
+        System.out.println("Человек выбрал, что ему  " + ageVersion2 + "лет");
+        if (age < 18);
+            System.out.println("Ваш возраст не достиг совершеннолетия, нужно немного подождать!\n");
+            //Task Number 2
+        System.out.println("Task Number 2\n");
+        //
+        //
+        int schoolAge = 7;
+        System.out.println("Если ребенку  " + schoolAge + "  лет");
+        if (schoolAge >= 7);
+        System.out.println("может пойти в школу");
+        int universityAge = 18;
+        System.out.println("Если человеку " + universityAge + "  лет");
+        if (universityAge >= 18);
+        System.out.println("может поступать в университет");
+        int workingAge = 24;
+        System.out.println("Если человеку " + workingAge + "  лет");
+        if (workingAge >= 24);
+        System.out.println("может искать работу\n");
+        //
+        //
+        //Task Number 3
+        System.out.println("Task Number 3\n");
+        // Вместимость одного вагона поезда составляет 102 человека. Вагон рассчитан на 60 сидячих мест, все остальные
+        // – стоячие.Напишите программу, которая выводит в консоль сообщение о том, есть ли место в вагоне:
+        // сидячее или стоячее или вагон уже полностью забит.
+        int totalCapacity = 102;
+        System.out.println("Звпрос на  " + totalCapacity + "  мест");
+        if (totalCapacity<= 102);
+        System.out.println("Есть место в вагоне");
+        int totalCapacityofTrain = 200;
+        System.out.println("Запрос на  " + totalCapacityofTrain + "  мест");
+        System.out.println("Вагон забит");
+        int seatPlace = 60;
+        System.out.println("Запрос на  " + seatPlace + "  мест");
+        if (seatPlace <= 60);
+        System.out.println("Имеются сидячие места");
+        int seatPlacebooked = 85;
+        System.out.println("Запрос на  " + seatPlacebooked + "  мест");
+        if (seatPlacebooked > 60);
+        System.out.println("Не имеются сидячие места");
+        int standingSeatscapacity = totalCapacity - seatPlace;
+        System.out.println("Если общее количество места  " + totalCapacity + "  и сидячих мест  " + seatPlace + "  , то стоячих мест -  " + standingSeatscapacity);
+        if (standingSeatscapacity <= 42);
+        System.out.println("имеются стоячие места");
+        int standingSeatsnumber = 50;
+        System.out.println("Запрос на  " + standingSeatsnumber + "  мест");
+        if ( standingSeatsnumber > 42);
+        System.out.println("Не имеются стоячие места");
+
+
+
+    }
+    }
+
+
---
+++ src/Part3.java
@@ -0,0 +1,45 @@
+public class Part3 {
+    public static void main(String[] args) {
+        //Task Number 1
+        System.out.println("Task Number 1\n");
+int ageOfperson = 25;
+        System.out.println("Человеку   " + ageOfperson + "  лет");
+        if (ageOfperson >= 2 && ageOfperson<6) {
+            System.out.println("Если возраст человека равен  " +ageOfperson + "   нужно ходить в детский сад");
+        }else if (ageOfperson >= 7 && ageOfperson <18) {
+            System.out.println("Если возраст человека равен  " +ageOfperson + "   нужно ходить в школу");
+        } else if (ageOfperson > 18 && ageOfperson < 24) {
+            System.out.println("Если возраст человека равен  " +ageOfperson + "   нужно идти в университет");
+        } else if (ageOfperson > 24) {
+            System.out.println("Если возраст человека равен  " +ageOfperson + "   нужно идти на работу\n");
+            //Task Number 2
+            System.out.println("Task Number 2\n");
+            //
+            //
+            int ageOfkid = 10;
+            System.out.println("Ребенку   " + ageOfkid + "  лет");
+            if (ageOfkid< 5) {
+                System.out.println("Ребенку меньше 5 лет, то он не может кататься на аттракционе");
+            } else if (ageOfkid >= 5 && ageOfkid <14) {
+                System.out.println("Ребенок может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
+            }else if (ageOfkid > 14) {
+                System.out.println("может кататься без сопровождения взрослого\n") ;
+
+            //Task Number 3
+                System.out.println("Task Number 3\n");
+                int one = 7;
+                int two = 15;
+                int three = 27;
+                if (one > two && one > three) {
+                    System.out.println("число  " + one + "больше других");
+                } else if (two > three && two > one) {
+                    System.out.println("число  " + two + "больше других");
+                } else if ( three > one && three > two) {
+                    System.out.println("число  " + three + "больше других");
+                } else {
+                    System.out.println("Все числа равны");
+                }
+            }
+        }
+    }
+}
